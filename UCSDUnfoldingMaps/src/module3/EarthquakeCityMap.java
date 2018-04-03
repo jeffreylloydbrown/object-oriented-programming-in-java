594b81e725a67b82ce6b0dc66a11a6591c7694c5
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-04-03 17:42:06 -0500

    simple changes, no logic changes
    replace hardcoded constants with member variables.  Define colors I will eventually use.  Point to the data directory for working offline.

--- UCSDUnfoldingMaps/src/module3/EarthquakeCityMap.java
+++ UCSDUnfoldingMaps/src/module3/EarthquakeCityMap.java
@@ -24,8 +24,8 @@ import parsing.ParseFeed;
 /** EarthquakeCityMap
  * An application with an interactive map displaying earthquake data.
  * Author: UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
- * Date: July 17, 2015
+ * @author Jeff Brown
+ * Date: April 2, 2018
  * */
 public class EarthquakeCityMap extends PApplet {
 
@@ -40,11 +40,23 @@ public class EarthquakeCityMap extends PApplet {
 	// Less than this threshold is a minor earthquake
 	public static final float THRESHOLD_LIGHT = 4;
 
+	// Various colors we'll use.
+	private int black = color(0,0,0);
+	private int red = color(255,0,0);
+	private int yellow = color(255, 255,0);
+	private int blue = color(0,0,255);
+
 	/** This is where to find the local tiles, for working without an Internet connection */
-	public static String mbTilesString = "blankLight-1-3.mbtiles";
+	public static String mbTilesString = "data/blankLight-1-3.mbtiles";
 	
 	// The map
 	private UnfoldingMap map;
+
+	// map's size information.
+	private int mapX = 200;
+	private int mapY = 50;
+	private int mapWidth = 700;
+	private int mapHeight = 500;
 	
 	//feed with magnitude 2.5+ Earthquakes
 	private String earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
@@ -54,13 +66,13 @@ public class EarthquakeCityMap extends PApplet {
 		size(950, 600, OPENGL);
 
 		if (offline) {
-		    map = new UnfoldingMap(this, 200, 50, 700, 500, new MBTilesMapProvider(mbTilesString));
-		    earthquakesURL = "2.5_week.atom"; 	// Same feed, saved Aug 7, 2015, for working offline
+		    map = new UnfoldingMap(this, mapX, mapY, mapWidth, mapHeight, new MBTilesMapProvider(mbTilesString));
+		    earthquakesURL = "data/2.5_week.atom"; 	// Same feed, saved Aug 7, 2015, for working offline
 		}
 		else {
-			map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
+			map = new UnfoldingMap(this, mapX, mapY, mapWidth, mapHeight, new Google.GoogleMapProvider());
 			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
-			//earthquakesURL = "2.5_week.atom";
+			//earthquakesURL = "data/2.5_week.atom";
 		}
 		
 	    map.zoomToLevel(2);
@@ -105,11 +117,7 @@ public class EarthquakeCityMap extends PApplet {
 		Object magObj = feature.getProperty("magnitude");
 		float mag = Float.parseFloat(magObj.toString());
 		
-		// Here is an example of how to use Processing's color method to generate 
-	    // an int that represents the color yellow.  
-	    int yellow = color(255, 255, 0);
-		
-		// TODO (Step 4): Add code below to style the marker's size and color 
+		// TODO (Step 4): Add code below to style the marker's size and color
 	    // according to the magnitude of the earthquake.  
 	    // Don't forget about the constants THRESHOLD_MODERATE and 
 	    // THRESHOLD_LIGHT, which are declared above.
