9b28fe57d35e9aa2d89b1d1e074c0c9a9f013e8f
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-04-12 13:55:14 -0500

    Steps 1, 2 and 4 implemented by inserting CommonMarker into the class hierarchy.

--- UCSDUnfoldingMaps/src/module5/CityMarker.java
+++ UCSDUnfoldingMaps/src/module5/CityMarker.java
@@ -13,10 +13,10 @@ import processing.core.PGraphics;
  * @author Jeff Brown
  *
  */
-// TODO: Change SimplePointMarker to CommonMarker as the very first thing you do 
+// Change SimplePointMarker to CommonMarker as the very first thing you do
 // in module 5 (i.e. CityMarker extends CommonMarker).  It will cause an error.
 // That's what's expected.
-public class CityMarker extends SimplePointMarker {
+public class CityMarker extends CommonMarker {
 	
 	public static int TRI_SIZE = 5;  // The size of the triangle marker
 	
@@ -35,7 +35,7 @@ public class CityMarker extends SimplePointMarker {
 	/**
 	 * Implementation of method to draw marker on the map.
 	 */
-	public void draw(PGraphics pg, float x, float y) {
+	public void drawMarker(PGraphics pg, float x, float y) {
 		// Save previous drawing style
 		pg.pushStyle();
 		
